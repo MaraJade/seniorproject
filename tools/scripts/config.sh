8b86b0d0e4fb19a7a095c20deb2b02350cb61a9c
Hai <nguyehai@onid.oregonstate.edu>
2016-03-07 18:27:12 -0800

    Insert branch for paper

---
+++ tools/scripts/config.sh
@@ -0,0 +1,13 @@
+# Docker Host names
+DEV_MACHINE_NAME=tutorialDev
+
+STAGE_MACHINE_NAME=tutorialStage
+STAGE_PORT=8888
+
+PROD_MACHINE_NAME=tutorialProd
+
+# Variables needed to workaround bug in folder sharing
+# on Windows, see configureDevHyper-v.sh
+USER_NAME=HaiTN
+CLIENT_IP=10.249.2.31
+CLIENT_PATH_TO_PROJECT=Users/HaiTN/csProject/seniorproject/tools/scripts/
